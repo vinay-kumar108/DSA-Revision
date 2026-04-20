d27785fc1dd696b3f3b1aca72a04e86d1b84624c
vinay-kumar108 <vinay20010920@gmail.com>
2026-04-20 22:49:22 +0530

    all patterns done

--- LogicBuilding/pattern20.cpp
+++ LogicBuilding/pattern20.cpp
@@ -5,14 +5,14 @@ int main(){
     cout<<"Enter a Number:";
     cin>>n;
     for(int i=0;i<2*n-1;i++){
-        for(int j=0;j<=n;j++){
-            if(i==0 || i==n-1){
-                cout<<n;
-            }
-            if(j==0 || j==n-1){
-                cout<<n;
-            }
-            n--;
+        for(int j=0;j<2*n-1;j++){
+           int top=i;
+           int bottom=2*n-1-i-1;
+           int left=j;
+           int right=2*n-j-2;
+           int x=min(top,bottom);
+           int y=min(left,right);
+           cout<<abs(min(x,y)-n);
         }
         cout<<endl;
     }
