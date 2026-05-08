d188d10b8dc748e7f6f4cee05a30dd2d359e9cd6
vinay-kumar108 <vinay20010920@gmail.com>
2026-05-08 23:19:40 +0530

    corrected approach

--- array/Remove Duplicates from Sorted Array.cpp
+++ array/Remove Duplicates from Sorted Array.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n=nums.size();
-        if(n<=1) return nums[0] ;
-        int i=0,j=1;
-        while(i<n&& j<n){
-            while(i<n-1 && j<n && nums[j]<=nums[i]){
-                j++;
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                swap(nums[i],nums[j]);
             }
-            swap(nums[i+1],nums[j]);
-            i++;
         }
-        return i;
-
-
-
+        return i+1;
     }
 };
